f66c07e3410f20bfcc2ab3c9e230d140493f3cc5
Guillermo <guillermojm173@gmail.com>
2024-11-18 20:48:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoZ',
+  webDir: 'www'
+};
+
+export default config;
